569a9e37e0cc003fc3c250c884cfa654e184e492
Li Lin <guru.lin@gmail.com>
2019-09-15 18:26:13 +0700

    use testify to shorten test cases

--- accounts/app/server/account_server_test.go
+++ accounts/app/server/account_server_test.go
@@ -2,6 +2,7 @@ package server
 
 import (
 	"context"
+	"github.com/stretchr/testify/assert"
 	"testing"
 
 	api_pb "github.com/sloppycoder/ngms/accounts/api"
@@ -11,32 +12,21 @@ func Test_AccountServiceServer_GetAccount(t *testing.T) {
 	svr := NewAccountServiceServer()
 
 	ctx := context.Background()
-	req := &api_pb.GetAccountRequest{
-		AccountId: "100",
-	}
+	req := &api_pb.GetAccountRequest{AccountId: "100"}
 
 	resp, err := svr.GetAccount(ctx, req)
 
-	if err != nil {
-		t.Errorf("returned an error %v", err)
-	}
-
-	if resp == nil {
-		t.Error("response should not nil")
-	}
+	assert.NotNil(t, resp)
+	assert.Nil(t, err)
 }
 
 func Test_AccountServiceServer_GetAccount_Invalid_AccountId(t *testing.T) {
 	svr := NewAccountServiceServer()
 
 	ctx := context.Background()
-	req := &api_pb.GetAccountRequest{
-		AccountId: "",
-	}
+	req := &api_pb.GetAccountRequest{AccountId: ""}
 
 	_, err := svr.GetAccount(ctx, req)
 
-	if err == nil {
-		t.Errorf("should have returend an error %v", err)
-	}
+	assert.NotNil(t, err)
 }
--- accounts/go.mod
+++ accounts/go.mod
@@ -6,6 +6,7 @@ require (
 	github.com/golang/protobuf v1.2.0
 	github.com/grpc-ecosystem/grpc-gateway v1.8.5
 	github.com/izumin5210/grapi v0.4.2
+	github.com/stretchr/testify v1.4.0
 	golang.org/x/net v0.0.0-20190827160401-ba9fcec4b297
 	google.golang.org/genproto v0.0.0-20180817151627-c66870c02cf8
 	google.golang.org/grpc v1.23.1
