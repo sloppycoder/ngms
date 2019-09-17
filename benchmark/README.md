555f981f6579ea21c6e0af0a15025cd3bd6a7561
Li Lin <guru.lin@gmail.com>
2019-09-17 21:18:34 +0700

    simple benchmark skeleton

---
+++ benchmark/README.md
@@ -0,0 +1,16 @@
+## benchmark the API using locust
+
+Use [Locust.io](http://locust.io) to benchmark API.
+
+### install
+```
+ conda install -c conda-forge locust
+```
+
+### run
+```
+locust --host http://127.0.0.1:3000
+```
+
+
+
---
+++ benchmark/locustfile.py
@@ -0,0 +1,11 @@
+from locust import HttpLocust, TaskSet, task
+
+class InvokeAccountsApi(TaskSet):
+    @task(1)
+    def account_summary(self):
+        self.client.get("/accounts/100-1234-5577-891")
+
+class ApiStressTest(HttpLocust):
+    task_set = InvokeAccountsApi
+    min_wait = 100
+    max_wait = 100
