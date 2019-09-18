542592a2453ef5065208f179a52f06149afec037
Li Lin <guru.lin@gmail.com>
2019-09-18 12:54:57 +0700

    switch test account

--- benchmark/locustfile.py
+++ benchmark/locustfile.py
@@ -3,7 +3,7 @@ from locust import HttpLocust, TaskSet, task
 class InvokeAccountsApi(TaskSet):
     @task(1)
     def account_summary(self):
-        self.client.get("/accounts/100-1234-5577-891")
+        self.client.get("/accounts/100-1234-5577-890")
 
 class ApiStressTest(HttpLocust):
     task_set = InvokeAccountsApi
