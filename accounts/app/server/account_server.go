eb3f1994e513140eca03196369b87a310a1a8783
Li Lin <guru.lin@gmail.com>
2019-09-19 17:21:13 +0700

    handle document found error and return correct status code

--- accounts/app/server/account_server.go
+++ accounts/app/server/account_server.go
@@ -3,6 +3,7 @@ package server
 import (
 	"context"
 	"google.golang.org/grpc/grpclog"
+	"strings"
 
 	// "github.com/golang/protobuf/ptypes/empty"
 	"github.com/izumin5210/grapi/pkg/grapiserver"
@@ -29,16 +30,23 @@ type accountServiceServerImpl struct {
 
 func (s *accountServiceServerImpl) GetAccount(ctx context.Context, req *api_pb.GetAccountRequest) (*api_pb.Account, error) {
 	id := req.AccountId
+	// grpclog.Infof("getAccount for %s", id)
 	if "" == id || "0" == id {
 		grpclog.Infof("invalid account id: %s", id)
 		return nil, status.Error(codes.InvalidArgument, "Invalid account id")
 	}
 
 	acc, err := repo.GetAccountById(ctx, id)
-	if err != nil {
+	if err == nil {
+		return acc, nil
+	}
+
+	if strings.Contains(err.Error(), "no documents in result") {
+		grpclog.Infof("account %s not found", id)
+		return nil, status.Error(codes.NotFound, "account does not exist")
+	} else {
 		grpclog.Infof("unable to retrieve account summary for %s %s", id, err)
 		return nil, status.Error(codes.Unavailable, "Unable to retrieve account")
 	}
 
-	return acc, nil
 }
